3e9ad9aa3b9d3a4bcef0eb04a517ea27d817c926
Jason Shin <visualbbasic@gmail.com>
2019-01-14 00:23:57 +1100

    chore: adding a longer way of error

--- exceptions/src/main.rs
+++ exceptions/src/main.rs
@@ -1,8 +1,10 @@
+use std::io;
 use std::fs::File;
+use std::io::Read;
 use std::io::ErrorKind;
 
 fn main() {
-    let f = File::open("hello.txt");
+    // let f = File::open("hello.txt");
 
     /* let f = match f {
         Ok(file) => file,
@@ -12,7 +14,7 @@ fn main() {
     };
     println!("zz {:?}", f); */
     // Matching on different errors
-    let f = match f {
+    /* let f = match f {
         Ok(file) => file,
         Err(error) => match error.kind() {
             ErrorKind::NotFound => match File::create("hello.txt") {
@@ -21,5 +23,29 @@ fn main() {
             },
             other_error => panic!("There was a problem opening up the file {:?}", other_error)
         }
-    };
+    }; */
+
+    // Using unwrap
+    // let f = File::open("hello.txt").unwrap();
+
+    // Using expect: similar to unwrap but lets you choose a panic!
+    // let f = File::open("hello.txt").expect("Failed to open hello.txt!");
+
+    // Propagating errors -> longer way
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let f = File::open("hello.txt");
+
+        let f = match f {
+            Ok(file) => file,
+            Err(e) => return Err(e),
+        };
+
+        let mut s = String::new();
+
+        match f.read_to_string(&mut s) {
+            Ok(_) => Ok(s),
+            Err(e) => Err(e),
+        };
+    }
+
 }
