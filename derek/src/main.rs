9c4c03d117551cd0734ac7272f3992b9aa1b7dd4
Jason Shin <visualbbasic@gmail.com>
2019-01-17 23:24:21 +1100

    finished derek tutorial

--- derek/src/main.rs
+++ derek/src/main.rs
@@ -97,6 +97,110 @@ fn main() {
     println!("6 + 4 = {}", sum(6, 4));
 
     // Closures
+    let sum_nums = |x: i32, y: i32| x + y;
+    println!("{}", sum_nums(7, 8));
+
+    let num_ten = 10;
+    let add_10 = |x: i32| x + num_ten;
+    println!("5 + 10 = {}", add_10(5));
+
+    // References
+    let vect1 = vec![1, 2, 3];
+    let vect2 = vect1;
+    // println!("vect1[0] : {}", vect1[0]); // throws an error:  value borrowed here after move...
+    println!("vect1[0] : {}", vect2[0]);
+
+    let prim_val = 1;
+    let prim_val2 = prim_val;
+    println!("prim_val : {}", prim_val);
+
+    println!("Sum of vect : {}", sum_vects(&vect2));
+
+    println!("Vect 2 : {:?}", &vect2);
+
+    // Struct => used to define custom data types
+    let mut circle1 = Circle {
+        x: 10.0,
+        y: 10.0,
+        radius: 10.0,
+    };
+
+    println!("x {}, y {}, radius {}", circle1.x, circle1.y, circle1.radius);
+    println!("Circle radius {}", get_radius(&circle1));
+    println!("Circle radius {}", circle1.get_x());
+
+    // structs
+    let mut rect1 = Rectangle {
+        height: 10.1,
+        width: 12.2,
+    };
+    println!("Rect radius {}", rect1.area());
+
+    // enum
+    let hulk = Hero::Strong(1234);
+    let quicksilver = Hero::Fast;
+    let spiderman = Hero::Info {name: "Spiderman".to_owned(), secret: "Pter Parker".to_owned(), };
+
+    get_info(hulk);
+    get_info(spiderman);
+}
+
+struct Circle {
+    x: f64,
+    y: f64,
+    radius: f64,
+}
+
+fn get_radius(circle: &Circle) -> f64 {
+    circle.radius
+}
+
+impl Circle {
+    pub fn get_x(&self) -> f64 {
+        self.x
+    }
+}
+
+enum Hero {
+    Fast,
+    Strong(i32),
+    Info { name: String, secret: String }
+}
+
+fn get_info(h: Hero) {
+    match h {
+        Hero::Fast => println!("Fast!"),
+        Hero::Strong(i) => println!("Lifts {}", i),
+        Hero::Info {name, secret} => {
+            println!("{} is {}", name, secret);
+        }
+    }
+}
+
+fn sum_vects(v1: &Vec<i32>) -> i32 {
+    let sum = v1.iter().fold(0, |mut sum, &x| {sum += x; sum});
+    return sum;
+}
+
+struct Rectangle {
+    height: f64,
+    width: f64,
+}
+
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
+impl HasArea for Circle {
+    fn area(&self) -> f64 {
+        3.14159 * (self.radius * self.radius)
+    }
+}
+
+impl HasArea for Rectangle {
+    fn area(&self) -> f64 {
+        self.width * self.height
+    }
 }
 
 fn say_hello(name: &str) {
