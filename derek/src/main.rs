8927b8661fffa9e7249a555963eb4258f6e87e8c
Jason Shin <visualbbasic@gmail.com>
2019-01-17 22:23:20 +1100

    chore: adding the guess game

--- derek/src/main.rs
+++ derek/src/main.rs
@@ -1,3 +1,6 @@
+use std::{ i32 };
+use std::io::stdin;
+
 fn main() {
     let random_string = "jason shin test";
     println!("Hello, world! length {}", random_string.len());
@@ -16,4 +19,55 @@ fn main() {
         indiv_char = chars.next();
     }
 
+    let mut iter = random_string.split_whitespace();
+    let mut indiv_word = iter.next();
+
+    loop {
+        match indiv_word {
+            Some(x) => println!("Indiv {}", x),
+            None => break,
+        }
+        indiv_word = iter.next();
+    }
+
+    let rand_string2 = "I am a string string string string string string string string string\nhaha haha\nhey hey hey";
+
+    let mut lines = rand_string2.lines();
+    let mut indiv_line = lines.next();
+
+    loop {
+        match indiv_line {
+            Some(x) => println!("{}", x),
+            None => break,
+        }
+        indiv_line = lines.next();
+    }
+
+    println!("Find best {}", rand_string2.contains("best"));
+
+
+    'outer: loop {
+        let number: i32 = 10;
+        println!("Pick a number");
+
+        loop {
+            let mut line = String::new();
+            let input = stdin().read_line(&mut line);
+
+            let guess: Option<i32> = input.ok().map_or(None, |_| line.trim().parse().ok());
+
+            match guess {
+                None => println!("Enter a Number"),
+                Some(n) if n == number => {
+                    println!("You guessed it!");
+                    break 'outer;
+                }
+                Some(n) if n < number =>
+                println!("Too low"),
+                Some(n) if n > number =>
+                println!("Too low"),
+                Some(_) => println!("Error")
+            }
+        }
+    }
 }
