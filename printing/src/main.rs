8156582b09907edc03dc68df4f981df1e10402dd
Jason Shin <visualbbasic@gmail.com>
2021-04-04 11:09:02 +1000

    print pointer address

--- printing/src/main.rs
+++ printing/src/main.rs
@@ -7,4 +7,16 @@ fn main() {
 
     println!("{:?}", '행' as u32);
     println!("\u{D589}");
+
+    // print out memory address
+    let p = &3;
+    let r = &p;
+    let r2 = &&p;
+    let r3 = &&&p;
+    let r4 = &&&&p;
+    println!("{:p}", p);
+    println!("{:p}", r);
+    println!("{:p}", r2);
+    println!("{:p}", r3);
+    println!("{:p}", r4);
 }
